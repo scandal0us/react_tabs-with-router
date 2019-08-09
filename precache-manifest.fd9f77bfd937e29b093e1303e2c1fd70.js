2068b17125f5b3cc61158b2bbed41f5b874decbd
scandal0us <d.l.lutsak@gmail.com>
2019-08-09 12:27:19 +0300

    Updates

---
+++ precache-manifest.fd9f77bfd937e29b093e1303e2c1fd70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947b040d399823027dd5dff25d922e3f",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "b8d6ec41a63c4be1923b",
+    "url": "/react_tabs-with-router/static/css/main.5f648b94.chunk.css"
+  },
+  {
+    "revision": "ca6138fd01b189979d21",
+    "url": "/react_tabs-with-router/static/js/2.e7222bf0.chunk.js"
+  },
+  {
+    "revision": "b8d6ec41a63c4be1923b",
+    "url": "/react_tabs-with-router/static/js/main.27a1332c.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
